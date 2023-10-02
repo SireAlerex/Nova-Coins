324dd4473b2acdeb749ee11cb2d386cacc77e41c
Axel <axel-bertrand@hotmail.fr>
2023-10-02 23:24:41 +0200

    1.20.1 fabric mod template

---
+++ src/main/java/alerex/novacoins/NovaCoinsDataGenerator.java
@@ -0,0 +1,11 @@
+package alerex.novacoins;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NovaCoinsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
