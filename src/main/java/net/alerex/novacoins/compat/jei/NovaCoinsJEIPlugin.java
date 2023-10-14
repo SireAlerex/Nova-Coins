e8df75de321b3f2345609d3b59d04ac92b70d4df
Axel <axel-bertrand@hotmail.fr>
2023-10-14 16:12:00 +0200

    start of jei compat

---
+++ src/main/java/net/alerex/novacoins/compat/jei/NovaCoinsJEIPlugin.java
@@ -0,0 +1,10 @@
+package net.alerex.novacoins.compat.jei;
+
+import mezz.jei.api.IModPlugin;
+
+public class NovaCoinsJEIPlugin  implements IModPlugin {
+	@Override
+	public net.minecraft.resources.ResourceLocation getPluginUid() {
+		return null;
+	}
+}
