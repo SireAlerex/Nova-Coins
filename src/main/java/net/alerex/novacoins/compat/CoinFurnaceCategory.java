6f1643bb522abb2146d8f25ace26fc3c56ca5f9c
Axel <axel-bertrand@hotmail.fr>
2023-11-09 12:32:59 +0100

    fix texture

--- src/main/java/net/alerex/novacoins/compat/CoinFurnaceCategory.java
+++ src/main/java/net/alerex/novacoins/compat/CoinFurnaceCategory.java
@@ -7,7 +7,6 @@ import me.shedaniel.rei.api.client.gui.widgets.Widget;
 import me.shedaniel.rei.api.client.gui.widgets.Widgets;
 import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.alerex.novacoins.NovaCoins;
 import net.alerex.novacoins.block.ModBlocks;
@@ -18,7 +17,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CoinFurnaceCategory implements DisplayCategory<CoinFurnaceDisplay> {
-	public static final Identifier TEXTURE = new Identifier(NovaCoins.MOD_ID, "textures/gui/coin_furnace.png");
+	public static final Identifier TEXTURE = new Identifier(NovaCoins.MOD_ID, "textures/gui/coin_furnace_rei.png");
 	public static final CategoryIdentifier<CoinFurnaceDisplay> COIN_FURNACE = CategoryIdentifier.of(NovaCoins.MOD_ID, "coin_furnace");
 
 	@Override
@@ -38,24 +37,35 @@ public class CoinFurnaceCategory implements DisplayCategory<CoinFurnaceDisplay> 
 
 	@Override
 	public List<Widget> setupDisplay(CoinFurnaceDisplay display, Rectangle bounds) {
-		final Point startPoint = new Point(bounds.getCenterX() -87, bounds.getCenterY() -35);
+		final Point startPoint = new Point(bounds.getCenterX() -(175/2), bounds.getCenterY() -(24));
 		List<Widget> widgets = new LinkedList<>();
 		widgets.add(Widgets.createRecipeBase(bounds));
 
-		widgets.add(Widgets.createTexturedWidget(TEXTURE, new Rectangle(startPoint.x, startPoint.y, 175, 82)));
+		widgets.add(Widgets.createTexturedWidget(TEXTURE, new Rectangle(startPoint.x, startPoint.y, 176, 49)));
 
-		widgets.add(Widgets.createSlot(new Point(startPoint.x +80, startPoint.y +11))
-				.entries(display.getInputEntries().get(0))
-				.markInput());
-		widgets.add(Widgets.createSlot(new Point(startPoint.x +80, startPoint.y +59))
+		widgets.add(Widgets.createBurningFire(new Point(startPoint.x	 + 56, startPoint.y + 30)).animationDurationMS(10000));
+
+		widgets.add(Widgets.createArrow(new Point(startPoint.x +79, startPoint.y +19)).animationDurationMS(5000));
+
+		widgets.add(Widgets.createLabel(new Point(bounds.x + bounds.width - 5, bounds.y + 5),
+				Text.translatable("label.novacoins.coin_furnace_rei"))
+					.noShadow()
+					.rightAligned()
+					.color(0xFF404040, 0xFFBBBBBB));
+
+		widgets.add(Widgets.createSlot(new Point(startPoint.x +56, startPoint.y +11))
 				.entries(display.getOutputEntries().get(0))
-				.markOutput());
+				.markInput());
+		widgets.add(Widgets.createSlot(new Point(startPoint.x +116, startPoint.y +19))
+				.entries(display.getOutputEntries().get(1))
+				.markOutput()
+				.disableBackground());
 
 		return widgets;
 	}
 
-//	@Override
-//	public int getDisplayHeight() {
-//		return 90;
-//	}
+	@Override
+	public int getDisplayHeight() {
+		return 49;
+	}
 }
